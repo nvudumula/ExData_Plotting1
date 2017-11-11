edf99689ac6577432677bb3ce49bb6dfa2895821
nvudumula <vvnrpers@yahoo.com>
2017-11-10 16:11:17 -0800

    First Commit after implementation
    Loaded the files using GitHub Upload functionality.

---
+++ plot1.R
@@ -0,0 +1,57 @@
+## Read data from input file and store it into Data Frame
+## Write the data frame to an output file for future use
+## Once output file is read, it signifiacntly improves the performance
+
+## Please note input file is sorted on the date, so we only need to read until the desired dates and leave out the records after that
+
+readData <- function() {
+	i <- 2;
+	j <- 1;
+	options(stringsAsFactors = FALSE)
+	df <- data.frame()
+	
+	f <- fread("./data/household_power_consumption.txt", sep= "\n", header = TRUE)
+	rows <- nrow(f);
+	colNamesList <- strsplit (colnames (f),";")
+	colNames <- c(colNamesList[[1]])
+	print (colNames)
+	while ( i <= rows ) {
+		recList <- strsplit(as.character(f[i,1]),";")
+		rec <- c(recList[[1]])
+
+		if ( (as.Date(rec[[1]][1],"%d/%m/%Y")) >= as.Date("02/01/2007","%m/%d/%Y") & (as.Date(rec[[1]][1],"%d/%m/%Y")) <= as.Date("02/02/2007","%m/%d/%Y")) {
+			
+			print (rec)
+			df <- rbind(df,rec)	
+			j <- j + 1
+		}
+		if ( (as.Date(rec[[1]][1],"%d/%m/%Y")) > as.Date("03/01/2007","%m/%d/%Y") ) {
+			break
+		}
+		i <- i+1;
+	}
+	names(df)[] <- colNames
+	print(nrow(df))
+	write.table(df, file = "./data/output.csv",row.names=FALSE, na="", sep=",")
+	df
+}
+
+## Function to draw the graph(s)
+
+drawGraphPlot1 <- function() {
+		
+	## if the data for the desired dates exists in the output file then take it from output file
+	## Otherwise, read the data from original input file
+
+	if(file.exists("./data/output.csv")){
+		df <- read.csv("./data/output.csv")
+	}
+	else {
+		df <- readData()
+	}
+
+	## Draw charts
+
+	hist(as.numeric(df$Global_active_power), col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
+
+}
